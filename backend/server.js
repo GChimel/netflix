76473d2bc9b5ac50b0bc8e6881e163def9b1b49d
GChimel <gustavochimel12@gmail.com>
2024-11-25 21:49:53 -0300

    feat: trending movies

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 
 import authRoutes from './routes/auth-route.js';
+import movieRoutes from './routes/movie-route.js';
+
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 
@@ -10,6 +12,7 @@ const app = express();
 app.use(express.json()); // Allow parse json requests
 
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/movie', movieRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
